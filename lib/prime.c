08e93d2591704eb4241a0412e9a19dd06d03d65a
Tengfei Tsui <tioover@gmail.com>
2014-07-22 17:36:40 +0800

    Update prime.c

--- lib/prime.c
+++ lib/prime.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define MAX 80000000
 
-int *prime(int max) {
-    int i, j, *p=malloc(sizeof(int) * max + 1);
-    //init
-    for (i = 1; i <= max; i++)
-        p[i] = 1;
-    //sieve
-    for (i = 2; i <= (int)floor(sqrt(max)); i++)
-        if (i != 0)
-            for (j = 2; i*j <= max; j++)
-                p[i*j] = 0;
-    j = 0;
-    for (i = 2; i <= max; i++)
-        if (p[i] == 1)
-            p[j++] = i;
-    p[j] = 0;
-    return p;
+int prime_n(int n) {
+	int i, j, m;
+	char *p=malloc(sizeof(char) * MAX + 1);
+	//init
+	for (i = 1; i <= MAX; i++)
+		p[i] = 1;
+	//sieve
+	m = 1;
+	for (i = 3; i <= MAX; i+=2) {
+		if (p[i]) {
+			if (++m == n)
+				return i;
+			for (j = i*2; j <= MAX; j+=i) {
+				p[j] = 0;
+			}
+		}
+	}
 }
 
-int is_prime(int n) {
-    int i, *p = prime(n);
-    for (i=0; p[i] != 0; i++);
-    return p[i-1];
+int main() {
+	printf("%d\n", prime_n(4263116));
 }
